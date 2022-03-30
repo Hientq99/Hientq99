1f5ac04642188a8c61f5c4332cf87a3b2dfcf6f6
Hientq99 <102671194+Hientq99@users.noreply.github.com>
2022-03-30 20:35:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Demo",
-    introTitle: 'Nguyễn Ngọc Anh',
+    introTitle: 'Người anh iu',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
