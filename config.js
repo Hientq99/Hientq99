621045215e6cdd9a79a0ba63c66ca49e13361848
Hientq99 <102671194+Hientq99@users.noreply.github.com>
2022-03-30 20:42:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -16,4 +16,4 @@ const CONFIG = {
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/hienhoi123/' //link mess của các bạn. VD:https://www.facebook.com/hienhoi123/
+    messLink: 
